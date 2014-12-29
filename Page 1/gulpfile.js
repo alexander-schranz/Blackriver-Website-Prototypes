c4e6aec5220212eeb1ef59bf94ad7e85974870bb
Alexander Schranz <alexander.schranz@massiveart.com>
2014-12-29 18:32:51 +0100

    minify css

--- Page 1/gulpfile.js
+++ Page 1/gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('styles', function () {
         //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('css'))
         .pipe(rename({suffix: ''}))
-        //.pipe(minifycss())
+        .pipe(minifycss())
         .pipe(gulp.dest('css'))
         .pipe(notify({ message: 'Styles task complete' }));
 });
